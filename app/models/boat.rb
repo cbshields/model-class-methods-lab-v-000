b41a99a4edb762b69cfa035b70b13f32d2778964
Caroline Shields <cbshields@outlook.com>
2018-04-17 21:25:48 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,19 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
-end
+
+  def self.first_five
+    all.limit(5)
+  end
+
+  def self.dinghy
+    where("length <?",20)
+  end
+
+  def self.ship
+    where("length >=?",20)
+  end
+
+
+  
+end #ends Boat class

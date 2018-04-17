0bcb916eeb43c2b32f0e1068b44e43ea3dfd6062
Caroline Shields <cbshields@outlook.com>
2018-04-17 22:23:31 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,20 +2,19 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    
+
   end
 
   def self.sailors
-    
+
   end
 
   def self.talented_seafarers
-    
+
   end
 
   def self.non_sailors
-    
+
   end
 
 end #ends Captain class
-

324e323c97c0250f4e20d1b8324120d282ae481d
Caroline Shields <cbshields@outlook.com>
2018-04-18 18:56:33 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,7 +2,7 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    includes(:classifications).where(classifications: {name: "catamaran"})
+    includes(:boats :classifications).where(classifications: {name: "Catamaran"})
   end
 
   def self.sailors

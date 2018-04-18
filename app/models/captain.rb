45a27a548c8c34c51e4d749ac675cbfb470c6694
Caroline Shields <cbshields@outlook.com>
2018-04-18 19:40:18 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -17,10 +17,8 @@ class Captain < ActiveRecord::Base
     where("id IN ?", self.sailors.pluck(:id) & self.motorboats.pluck(:id))
   end
 
-
-
   def self.non_sailors
-
+    where.not("id IN ?", self.sailors.pluck(:id))
   end
 
 end #ends Captain class
